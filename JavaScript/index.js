e6187151888fa3cbfd6abc519219399f6a8827a7
RosyJu <755126039@qq.com>
2025-03-29 00:18:23 +0800

    test

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,22 +1,75 @@
 //获取日期的DOM
-const dateday = document.querySelectorAll("div.date > div.day > span.textday.text")[0]
-const datehours = document.querySelectorAll("div.date > div.time > span.texthours.text")[0]
-const dateminute = document.querySelectorAll("div.date > div.time > span.textminute.text")[0]
-const datesecond = document.querySelectorAll("div.date > div.time > span.textsecond.text")[0]
+const dateday = document.querySelectorAll(
+  "div.date > div.day > span.textday.text"
+)[0];
+const datehours = document.querySelectorAll(
+  "div.date > div.time > span.texthours.text"
+)[0];
+const dateminute = document.querySelectorAll(
+  "div.date > div.time > span.textminute.text"
+)[0];
+const datesecond = document.querySelectorAll(
+  "div.date > div.time > span.textsecond.text"
+)[0];
 
-let date = setInterval(function() {
-    // 计算时间戳起点终点时间段的时间戳差距
-    const start = 1729869000
-    let end = Math.floor(Date.now() / 1000)
-    let TimePeriod = end - start // 差距
-    // 计算单位时间差
-    let day = Math.floor(TimePeriod / 86400) // 天
-    let hours = Math.floor( (TimePeriod - ( day * 86400 ) ) / 3600 ) // 时
-    let minute = Math.floor( (TimePeriod - ( day * 86400 + hours * 3600 ) ) / 60) // 分
-    let second = Math.floor( (TimePeriod - ( day * 86400 + hours * 3600 + minute * 60) )) // 秒
-    // 页面插入已完成的时间
-    dateday.textContent = day
-    datehours.textContent = hours.toString().padStart(2, '0')
-    dateminute.innerHTML = `&ensp;${minute.toString().padStart(2, '0')}`
-    datesecond.innerHTML = `&ensp;${second.toString().padStart(2, '0')}`
-},200)
+let date = setInterval(function () {
+  // 计算时间戳起点终点时间段的时间戳差距
+  const start = 1729869000; // 2024.12.25T23:10+0800
+  let end = Math.floor(Date.now() / 1000);
+  let TimePeriod = end - start; // 差距
+  // 计算单位时间差
+  let day = Math.floor(TimePeriod / 86400); // 天
+  let hours = Math.floor((TimePeriod - day * 86400) / 3600); // 时
+  let minute = Math.floor((TimePeriod - (day * 86400 + hours * 3600)) / 60); // 分
+  let second = Math.floor(
+    TimePeriod - (day * 86400 + hours * 3600 + minute * 60)
+  ); // 秒
+  // 页面插入已完成的时间
+  dateday.textContent = day;
+  datehours.textContent = hours.toString().padStart(2, "0");
+  dateminute.innerHTML = `&ensp;${minute.toString().padStart(2, "0")}`;
+  datesecond.innerHTML = `&ensp;${second.toString().padStart(2, "0")}`;
+}, 200);
+
+let popupstextdiv = document.querySelectorAll("div.popupstext")[0];
+const popupstext = {
+  "License Agreement": `
+MIT License
+<br />
+<br />
+Copyright (c) 2025 RosyJu
+<br />
+<br />
+Permission is hereby granted, free of charge, to any person obtaining a copyof this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+<br />
+<br />
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+<br />
+<br />
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`,
+  "Project Address": `<a href="https://github.com/RosyJu/yyl" target="_blank">github.com/RosyJu/yyl</a>`,
+};
+
+const popupsSvg = function () {
+  if (document.querySelectorAll("div.popups")[0].style.display == "none") {
+    let title = this.textContent;
+    popupstextdiv.innerHTML = popupstext[title];
+    document.querySelectorAll(
+      "div.popups div.popupsPrefix > div.title"
+    )[0].textContent = title;
+    document.querySelectorAll("div.popups")[0].style.display = "block";
+  } else {
+    document.querySelectorAll("div.popups")[0].style.display = "none";
+  }
+};
+
+document
+  .querySelectorAll("div.popups > div.body > div.popupsPrefix > svg")[0]
+  .addEventListener("click", popupsSvg);
+document
+  .querySelectorAll("div.head div#License-Agreement")[0]
+  .addEventListener("click", popupsSvg);
+document
+  .querySelectorAll("div.head div#Project-Address")[0]
+  .addEventListener("click", popupsSvg);

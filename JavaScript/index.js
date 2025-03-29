af6027f32e3f3637dd3606d137ae15958c586207
RosyJu <755126039@qq.com>
2025-03-29 12:22:35 +0800

    test

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -47,7 +47,8 @@ The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 <br />
 <br />
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`,
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+<br /><br /><a href="https://github.com/RosyJu/yyl/blob/main/LICENSE" target="_blank">github.com/RosyJu/yyl/blob/main/LICENSE</a>`,
   "Project Address": `<a href="https://github.com/RosyJu/yyl" target="_blank">github.com/RosyJu/yyl</a>`,
 };
 
@@ -68,8 +69,8 @@ document
   .querySelectorAll("div.popups > div.body > div.popupsPrefix > svg")[0]
   .addEventListener("click", popupsSvg);
 document
-  .querySelectorAll("div.head div#License-Agreement")[0]
+  .querySelectorAll("div.head div#License-Agreement > span")[0]
   .addEventListener("click", popupsSvg);
 document
-  .querySelectorAll("div.head div#Project-Address")[0]
+  .querySelectorAll("div.head div#Project-Address > span")[0]
   .addEventListener("click", popupsSvg);

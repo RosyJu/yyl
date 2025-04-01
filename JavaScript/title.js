0769f8a0cce9ce05ca9e0fc26a396e769a2cad3d
RosyJu <755126039@qq.com>
2025-04-01 12:17:41 +0800

    404test

--- JavaScript/title.js
+++ JavaScript/title.js
@@ -1,4 +1,3 @@
-let title = [1,"你记得花,花就不会枯萎."];
-title[0] = Math.floor(Math.random() * (title.length - 1) + 1)
-document.querySelectorAll("head > title")[0].textContent =
-  title[title[0]];
+let title = [1, "你记得花,花就不会枯萎."];
+title[0] = Math.floor(Math.random() * (title.length - 1) + 1);
+document.querySelectorAll("head > title")[0].textContent = title[title[0]];

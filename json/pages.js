7870c8dcd47b5105c3ce75fb74dc172aa70e025d
RosyJu <755126039@qq.com>
2025-04-03 17:48:13 +0800

    2048 test

--- json/pages.js
+++ json/pages.js
@@ -1,3 +1,4 @@
 const pages = [
-  { enable: true, time: 2503262355, title: "可以成为我的恋人吗" }
+  { enable: true, time: 2503262355, title: "可以成为我的恋人吗" },
+  { enable: true, time: 2504031733, title: "2048小游戏"}
 ];
